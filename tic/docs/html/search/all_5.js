47fa3605c5f7361f11f28d38b7bc6ee0ce625ff4
anythingtom <71325886+anythingtom@users.noreply.github.com>
2021-11-30 21:42:49 +0700

    Delete tic/docs directory

--- tic/docs/html/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tic_2ecpp_8',['tic.cpp',['../tic_8cpp.html',1,'']]]
-];
--- tic/docs/html/search/variables_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['player1wins_14',['Player1Wins',['../tic_8cpp.html#a60c9a41715e0a88832d681e030a343c2',1,'tic.cpp']]],
-  ['player2wins_15',['Player2Wins',['../tic_8cpp.html#a088d247d8169f8d4943d6bf1e3263c1f',1,'tic.cpp']]]
-];
